627b1f499922ffdb293144818dd859275ff9ee4b
amir hossein jahangir <a.jahangir74@gmail.com>
2023-01-04 23:34:08 +0330

    Fix Home Index 3

--- src/pages/Home.vue
+++ src/pages/Home.vue
@@ -2,7 +2,7 @@
   <div class="container mt-5">
     <div class="row">
       <div class="col-md-8">
-        <p>this page is just for test and learning vuejs <br/> for check fake api users and posts click on buttons</p>
+        <p>this page is just for test and learning vuejs . <br/> for check fake api users and posts click on buttons</p>
           <router-link class="btn btn-dark" :to="{ name: 'users' }">Users</router-link>
       </div>
     </div>
