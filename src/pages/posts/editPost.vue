61b67f0fab9743d3131ff362000cabab301b8a98
amir hossein jahangir <a.jahangir74@gmail.com>
2023-01-08 02:26:05 +0330

    Creating Post Update Page

--- src/pages/posts/editPost.vue
+++ src/pages/posts/editPost.vue
@@ -1,6 +1,6 @@
 <template>
     <div class="col-md-6">
-        <h2 class="mb-5">Create Post :</h2>
+        <h2 class="mb-5">Edit Post :</h2>
         <form @submit.prevent="validate">
             <div class="mb-3">
                 <label class="form-label">Title</label>
@@ -14,7 +14,7 @@
             </div>
             <button type="submit" class="btn btn-dark" :disabled="loading">
               <div v-if="loading" class="spinner-border spinner-border-sm" role="status"></div>
-              Create Post
+              Edit Post
             </button>
         </form>
     </div>
@@ -24,14 +24,26 @@
 import Swal from 'sweetalert2'
 import axios from 'axios'
 import { reactive, ref } from 'vue'
+import { useRoute } from 'vue-router'
 
+const loading = ref(false)
+const route = useRoute()
 const form = reactive({
   title: '',
   titleError: '',
   body: '',
   bodyError: ''
 })
-const loading = ref(false)
+
+axios
+  .get(`https://jsonplaceholder.typicode.com/posts/${route.params.id}`)
+  .then(function (response) {
+    form.title = response.data.title
+    form.body = response.data.body
+  })
+  .catch(function (error) {
+    console.log(error)
+  })
 
 function validate () {
   if (form.title === '') {
@@ -45,24 +57,23 @@ function validate () {
     form.bodyError = ''
   }
   if (form.title !== '' && form.body !== '') {
-    createPost()
+    updatePost()
   }
 }
 
-function createPost () {
+function updatePost () {
   loading.value = true
-  axios.post('https://jsonplaceholder.typicode.com/posts', {
+  axios.put(`https://jsonplaceholder.typicode.com/posts/${route.params.id}`, {
+    id: route.params.id,
     title: form.title,
     body: form.body,
     userId: 1
   })
     .then(function () {
-      form.title = ''
-      form.body = ''
       loading.value = false
       Swal.fire({
         title: 'Thanks!',
-        text: 'Post Created Successfully',
+        text: 'Post Updated Successfully',
         icon: 'success',
         confirmButtonText: 'Ok'
       })
