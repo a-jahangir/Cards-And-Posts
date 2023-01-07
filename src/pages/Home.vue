701216a699c314ab75b920af95c3792c9478c2cf
amir hossein jahangir <a.jahangir74@gmail.com>
2023-01-08 03:04:45 +0330

    Fix Home Index And Navbar Title

--- src/pages/Home.vue
+++ src/pages/Home.vue
@@ -2,7 +2,7 @@
   <div class="container mt-5">
     <div class="row">
       <div class="col-md-8">
-        <p>this page is just for test and learning vuejs . <br/> for check fake api users and posts click on buttons</p>
+        <p>this page is just for test and learning vuejs . <br/> for check fake api users and posts click on buttons .</p>
           <router-link class="btn btn-dark" :to="{ name: 'users' }">Users</router-link>
           <router-link class="btn btn-light ms-4" :to="{ name: 'posts' }">Posts</router-link>
       </div>
